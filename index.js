7bf481ab3e30f6827aa5bc5db3294b9ad77d93ee
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-08-31 17:31:01 -0400

    created fetchwrapper, api key, and endpoint for exchange api

---
+++ index.js
@@ -0,0 +1,9 @@
+import FetchWrapper from "./fetch-wrapper";
+
+const key = "4d46a45b4a63af8d1fe2edf8"
+
+const exchangeAPI = fetch(`https://v6.exchangerate-api.com/v6/${key}`)
+
+const endpoint = "/latest/USD" //USD as example for now
+
+
