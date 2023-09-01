11bca547542f4cfec836e7fa80f31d7997e7b9a9
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-08-31 21:09:05 -0400

    added country currency options

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import FetchWrapper from "./fetch-wrapper.js";
 
 const key = "4d46a45b4a63af8d1fe2edf8";
-const endpoint = "/latest/USD"; //USD as example for now
+const exchangeAPI = new FetchWrapper(`https://v6.exchangerate-api.com/v6/${key}`);
 
-const exchangeAPI = new FetchWrapper(`https://v6.exchangerate-api.com/v6/${key}`)
+const baseSelect = document.querySelector("#base-currency");
+const targetSelect = document.querySelector("#target-currency");
 
 
-exchangeAPI.get(endpoint)
-.then(data => {
-    console.log(data)
-})
+
+
+
+
 
 
