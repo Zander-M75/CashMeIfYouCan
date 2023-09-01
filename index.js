37e446064b09043734622405de13c98ae420bcbb
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-08-31 21:32:34 -0400

    added script change event to update currency

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ import FetchWrapper from "./fetch-wrapper.js";
 const key = "4d46a45b4a63af8d1fe2edf8";
 const exchangeAPI = new FetchWrapper(`https://v6.exchangerate-api.com/v6/${key}`);
 
-const baseSelect = document.querySelector("#base-currency");
-const targetSelect = document.querySelector("#target-currency");
 
 
+const base = document.querySelector("#base-currency");
+const target =  document.querySelector("#target-currency");
+const result =  document.querySelector("#target-result");
+
+const getConversion = () => {
+    exchangeAPI.get(`/latest/${base.value}`)
+    .then(data => {
+        console.log(data.conversion_rates)
+        result.textContent = data.conversion_rates[target.value]
+    })
+};
+
+base.addEventListener("change", () => {
+    getConversion()
+})
 
 
+target.addEventListener("change", () => {
+    getConversion()
+})
 
 
 
